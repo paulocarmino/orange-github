21be793d7b2fdd0c2a21ea0d665a7b51b7592371
Paulo Carmino <paulocarmino@Paulos-iMac.local>
2019-11-24 19:33:02 -0300

    Initial Version with Home Route Configured

---
+++ src/pages/Main/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import {View, Text} from 'react-native';
+
+const Main = () => (
+  <View>
+    <Text>Oi!</Text>
+  </View>
+);
+
+Main.navigationOptions = {
+  title: 'Home',
+};
+
+export default Main;
